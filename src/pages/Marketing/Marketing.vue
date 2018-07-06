4dcdaf43b9861549eb1c37cff45316ff84ec7e68
Roman Cervantes <romancm@gmail.com>
2018-07-05 17:01:39 -0700

    Marketing page clean up

--- src/pages/Marketing/Marketing.vue
+++ src/pages/Marketing/Marketing.vue
@@ -1,20 +1,11 @@
 <template lang="html">
-    <div>
-        <md-empty-state
-          md-icon="devices_other"
-          md-label="SwitchList"
-          md-description="Quickly and effortlessly manage your Nintendo Switch Collection">
-          <router-link :to="{ name: 'register' }">
-              <md-button class="md-accent md-raised">Create an account</md-button>
-          </router-link>
-        </md-empty-state>
-    </div>
+    <md-empty-state
+        md-icon="devices_other"
+        md-label="SwitchList"
+        md-description="Quickly and effortlessly manage your Nintendo Switch Collection"
+    >
+        <router-link :to="{ name: 'register' }">
+            <md-button class="md-accent md-raised">Create an account</md-button>
+        </router-link>
+    </md-empty-state>
 </template>
-
-<script>
-export default {
-};
-</script>
-
-<style lang="scss" rel="stylesheet/scss" scoped>
-</style>
