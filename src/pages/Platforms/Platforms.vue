e8bbcc29efa800d2ad11adc6bcac1924501fb5ec
Roman Cervantes <romancm@gmail.com>
2018-11-01 18:43:31 -0700

    Added more platforms

--- src/pages/Platforms/Platforms.vue
+++ src/pages/Platforms/Platforms.vue
@@ -2,10 +2,16 @@
     <div class="platforms-page">
         <h2>Choose a platform</h2>
 
+        Sort: <select v-model="sortBy">
+            <option value="generations">Console Generation</option>
+            <option value="alphabetically">Alphabetically</option>
+        </select>
+
         <div class="platforms">
             <a
                 v-close-popover
-                v-for="platform in platforms"
+                v-for="platform in sorted"
+                v-if="sortBy === 'alphabetically'"
                 :key="platform.name"
                 :style="`background-color: ${platform.hex}`"
                 @click="changePlatform(platform)"
@@ -16,28 +22,77 @@
                 />
             </a>
         </div>
+
+        <div class="generations" v-if="sortBy === 'generations'">
+            <div v-for="(group, generation) in groupedByGenerations" :key="generation">
+                <h2>{{ ordinalSuffix(generation) }} generation</h2>
+
+                <div class="platforms">
+                    <a
+                        v-close-popover
+                        v-for="platform in group"
+                        :key="platform.name"
+                        :style="`background-color: ${platform.hex}`"
+                        @click="changePlatform(platform)"
+                    >
+                        <img
+                            :src='`/static/img/platforms/${platform.code}.svg`'
+                            :alt="platform.name"
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import platforms from '@/shared/platforms';
+import _ from 'lodash';
 import { mapState } from 'vuex';
 
 export default {
     data() {
         return {
             platforms,
+            sortBy: 'alphabetically',
         };
     },
 
     computed: {
         ...mapState(['gameLists', 'platform']),
+
+        groupedByGenerations() {
+            return _.groupBy(this.platforms, 'generation');
+        },
+
+        sorted() {
+            return _.sortBy(this.platforms, 'name');
+        },
     },
 
     methods: {
         changePlatform(platform) {
             this.$store.commit('SET_PLATFORM', platform);
             this.$router.push({ name: 'home' });
+        },
+
+        ordinalSuffix(value) {
+            const j = value % 10;
+            const k = value % 100;
+
+            if (j === 1 && k !== 11) {
+                return `${value}st`;
+            }
+
+            if (j === 2 && k !== 12) {
+                return `${value}nd`;
+            }
+            if (j === 3 && k !== 13) {
+                return `${value}rd`;
+            }
+
+            return `${value}th`;
         },
     },
 };
@@ -58,6 +113,7 @@ export default {
     }
 
     .platforms {
+        margin-top: $gp;
         display: grid;
         grid-template-columns: repeat(auto-fill, 140px);
         grid-gap: $gp;
@@ -77,5 +133,10 @@ export default {
             }
         }
 
+    }
+
+    .generations {
+        display: flex;
+        flex-direction: column-reverse;
     }
 </style>
