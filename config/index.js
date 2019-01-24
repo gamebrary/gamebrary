e43075cb04e11f2cb56105eb5a94a277718bd9c8
Roman Cervantes <romancm@gmail.com>
2019-01-23 23:13:03 -0700

    Changed port and auto open browser options

--- config/index.js
+++ config/index.js
@@ -14,8 +14,8 @@ module.exports = {
 
         // Various Dev Server settings
         host: 'localhost', // can be overwritten by process.env.HOST
-        port: 3000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-        autoOpenBrowser: false,
+        port: 5000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+        autoOpenBrowser: true,
         errorOverlay: true,
         notifyOnErrors: true,
         poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
