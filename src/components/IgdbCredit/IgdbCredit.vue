1c82ab32ba48241b042ffd85144355a9ae5bd8b7
Roman Cervantes <romancm@gmail.com>
2019-02-05 23:55:47 -0700

    IGDB credit component

---
+++ src/components/IgdbCredit/IgdbCredit.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+    <a :href="href" class="igdb-credit" target="_blank" :class="{ gray }">
+        <img :src="`/static/img/igdb-logo${logo}.svg`" />
+        <strong>Powered by IGDB</strong>
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        gray: Boolean,
+        linkable: Boolean,
+    },
+
+    computed: {
+        logo() {
+            return this.gray
+                ? '-gray'
+                : '';
+        },
+
+        href() {
+            return this.linkable
+                ? 'https://www.igdb.com/'
+                : null;
+        },
+    },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+    @import "~styles/styles.scss";
+
+    .igdb-credit {
+        color: $color-igdb-green;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        font-size: 11px;
+
+        &.gray {
+            color: $color-dark-gray;
+        }
+
+        img {
+            height: 40px;
+        }
+    }
+</style>
