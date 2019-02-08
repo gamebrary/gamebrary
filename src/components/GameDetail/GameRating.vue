31570e086d9875fd9070117c9bcf252495fdaaff
Roman Cervantes <romancm@gmail.com>
2019-02-07 23:07:40 -0700

    Remove numeric game rating in favor of star rating

--- src/components/GameDetail/GameRating.vue
+++ src/components/GameDetail/GameRating.vue
@@ -1,13 +1,11 @@
-<!-- eslint-disable -->
 <template lang="html">
-    <div :class="['game-rating', ratingClass, { 'small': small, dark: darkModeEnabled }]">
-        <span class="rating">
-            {{ formattedRating }}
-        </span>
+    <div
+        :class="['game-rating', { small, dark }]">
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="4 3 91 111">
-            <path d="M49 3L4 29v52l45 26 46-26V29zm0 95L12 77V34l37-21 38 21v43zm0 0" />
-        </svg>
+        <template v-for="n in 5">
+            <i class="fas fa-star" v-if="(roundedRating - n) + 1 >= 1" />
+            <i class="fas fa-star-half" v-if="(roundedRating - n) + 1 === .5" />
+        </template>
     </div>
 </template>
 
@@ -18,26 +16,19 @@ export default {
     props: {
         rating: Number,
         small: Boolean,
+        placeholder: Boolean,
     },
 
     computed: {
         ...mapGetters(['darkModeEnabled']),
 
-        ratingClass() {
-            const gameRating = Number(this.rating);
-            if (gameRating === 100) { return 'master'; }
-            if (gameRating >= 90) { return 'amazing'; }
-            if (gameRating >= 80) { return 'great'; }
-            if (gameRating >= 70) { return 'good'; }
-            if (gameRating >= 60) { return 'okay'; }
-            return 'bad';
+        roundedRating() {
+            return Math.round((this.rating / 20) * 2) / 2;
         },
 
-        formattedRating() {
-            return isNaN(this.rating)
-                ? '?'
-                : parseInt(this.rating, 10);
-        },
+        dark() {
+            return this.darkModeEnabled || this.placeholder;
+        }
     },
 };
 </script>
@@ -46,41 +37,15 @@ export default {
 @import "~styles/styles.scss";
 
 .game-rating {
-    width: 100px;
-    height: 115px;
-    position: relative;
+    color: $color-orange;
+    font-size: 20px;
 
     &.small {
-        margin-top: $gp / 2;
-        width: 34px;
-        height: 39px;
-
-        .rating {
-            font-size: 20px;
-        }
+        font-size: 12px;
     }
-
-    &.master { fill: #0e9246; color: #0e9246; }
-    &.amazing { fill: #7dbb42; color: #7dbb42; }
-    &.great { fill: #fecc09; color: #fecc09; }
-    &.good { fill: #f68e1e; color: #f68e1e; }
-    &.okay { fill: #ee4723; color: #ee4723; }
-    &.bad { fill: #bc2025; color: #bc2025; }
 
     &.dark {
-        fill: $color-dark-gray !important;
         color: $color-dark-gray !important;
-    }
-
-    .rating {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 48px;
-        font-weight: bold;
     }
 }
 </style>
