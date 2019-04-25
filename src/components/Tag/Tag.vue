8cc478d075003760d69e23cc1b9b249e01e9e707
Roman Cervantes <romancm@gmail.com>
2019-04-25 16:25:22 -0700

    Tag component

--- src/components/Tag/Tag.vue
+++ src/components/Tag/Tag.vue
@@ -1,24 +1,52 @@
 <template lang="html">
-    <button class="tag" v-if="label" :style="size">
-        <span >
-            <i class="fas fa-times close" v-if="!readonly" />
-        </span>
+    <span
+        v-if="label && hex"
+        class="tag"
+        :style="`background-color: ${hex}`"
+        :class="textColor"
+        @click="action"
+    >
+        <i class="fas fa-times close" @click="close" />
+
         {{ label }}
-    </button>
+    </span>
 </template>
 
 <script>
+import Tag from '@/components/Tag/Tag';
+
 export default {
+    components: {
+        Tag,
+    },
+
     props: {
-        readonly: {
-            type: Boolean,
-            default: true,
-        },
         label: String,
-        color: String,
-        size: {
-            type: String,
-            default: 'xsmall',
+        hex: String,
+    },
+
+    computed: {
+        textColor() {
+            const hexColor = this.hex ? this.hex.replace('#', 0) : '#000000';
+
+            const r = parseInt(hexColor.substr(0, 2), 16);
+            const g = parseInt(hexColor.substr(2, 2), 16);
+            const b = parseInt(hexColor.substr(4, 2), 16);
+
+            // eslint-disable-next-line
+            const yiq = ((r*299)+(g*587)+(b*114))/1000;
+
+            return yiq >= 128 ? 'dark' : 'light';
+        },
+    },
+
+    methods: {
+        action() {
+            this.$emit('action');
+        },
+
+        close() {
+            this.$emit('close');
         },
     },
 };
@@ -28,16 +56,38 @@ export default {
     @import "~styles/styles.scss";
 
     .tag {
-        background-color: $color-blue;
+        border-radius: 100px;
+        padding: 2px;
+        font-size: $font-size-xsmall;
+        display: flex;
         align-items: center;
-        justify-content: center;
+        color: $color-dark-gray;
+        cursor: pointer;
+
+        &.light { color: $color-white; }
+        &.dark { color: $color-darkest-gray; }
     }
 
     .close {
-        font-size: $font-size-xsmall;
-        margin-right: 2px;
-        background: $color-white;
-        border-radius: 100%;
+        height: 15px;
         width: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 100%;
+        margin-right: 4px;
+    }
+
+    .light .close {
+        background-color: $color-dark-gray-transparent;
+
+        &:hover {
+            color: $color-red;
+            background-color: $color-light-gray-transparent;
+        }
+    }
+
+    .dark .close {
+        background-color: $color-light-gray-transparent;
     }
 </style>
