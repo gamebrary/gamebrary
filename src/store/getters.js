b7533784879feb08d5518d1434790798bc8c3782
Roman Cervantes <romancm@gmail.com>
2019-06-08 18:52:36 -0700

    Moved ageRatings to getter + updated for v3

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,22 @@
 export default {
+    // eslint-disable-next-line
+    ageRatings: () => {
+        return {
+            1: '3',
+            2: '7',
+            3: '12',
+            4: '16',
+            5: '18',
+            6: 'RP',
+            7: 'EC',
+            8: 'E',
+            9: 'E10',
+            10: 'T',
+            11: 'M',
+            12: 'AO',
+        };
+    },
+
     developers: (state) => {
         const developers = state.game.developers;
 
