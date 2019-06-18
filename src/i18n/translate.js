db8ca2a538be1b0cb9edffe9f0d01e4097e17ace
Roman Cervantes <romancm@gmail.com>
2019-06-18 16:46:15 -0700

    Translate script

---
+++ src/i18n/translate.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const translate = require('translate-json-object')();
+const baseJson = require('./en.json');
+
+translate.init({ googleApiKey: 'GOOGLE_API_KEY_HERE' });
+
+const supportedLanguages = ['es', 'pl', 'de', 'ar', 'fr', 'it', 'eu', 'cs'];
+
+for (let i = 0, promise = Promise.resolve(); i < supportedLanguages.length; i++) {
+    promise = promise.then(() => new Promise(resolve => {
+        const language = supportedLanguages[i];
+
+        translate.translate(baseJson, language)
+            .then((data) => {
+                fs.writeFile(`${language}.json`, JSON.stringify(data, null, 4), () => {
+                    console.log(`Translation to ${language} completed`);
+                    resolve();
+                });
+            }).catch((err) => {
+                console.log('error ', err)
+            });
+    }));
+}
