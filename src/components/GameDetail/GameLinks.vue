d986c587d5ac9ef8cc82c354052fd5b93aa45fa0
Roman Cervantes <romancm@gmail.com>
2019-06-19 16:20:45 -0700

    Improved links section

--- src/components/GameDetail/GameLinks.vue
+++ src/components/GameDetail/GameLinks.vue
@@ -1,14 +1,27 @@
 <template lang="html">
-    <div class="links" v-if="hasWebsites">
-        <a
-            v-for="{ category, url } in game.websites"
-            :key="category"
-            :href="url"
-            target="_blank"
-        >
-            <i :class="getIcon(category)" />
-            {{ linkTypes[category] }}
-        </a>
+    <div class="game-links">
+        <h4>Links</h4>
+        <div class="links" v-if="hasWebsites">
+            <a
+                v-for="{ category, url } in game.websites"
+                :key="category"
+                :href="url"
+                :class="linkTypes[category].name"
+                target="_blank"
+            >
+                <template v-if="linkTypes[category].icon">
+                    <i
+                        :class="linkTypes[category].icon"
+                        :title="$t(`gameDetail.links.${linkTypes[category].name}`)"
+                    />
+                </template>
+
+                <template v-else>
+                    <i class="fas fa-link"/>
+                    <small>{{ $t(`gameDetail.links.${linkTypes[category].name}`) }}</small>
+                </template>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -18,34 +31,26 @@ import { mapState } from 'vuex';
 export default {
     data() {
         return {
-            icons: {
-                official: 'fas fa-globe-americas',
-                facebook: 'fab fa-facebook-f',
-                steam: 'fab fa-steam',
-                youtube: 'fab fa-youtube',
-                twitter: 'fab fa-twitter',
-                instagram: 'fab fa-instagram',
-                iphone: 'fab fa-app-store-ios',
-                wikipedia: 'fab fa-wikipedia-w',
-                wikia: 'fas fa-link',
-                twitch: 'fab fa-twitch',
-                Reddit: 'fab fa-reddit',
-                reddit: 'fab fa-reddit',
-            },
             linkTypes: {
-                1: 'official',
-                2: 'wikia',
-                3: 'wikipedia',
-                4: 'facebook',
-                5: 'twitter',
-                6: 'twitch',
-                8: 'instagram',
-                9: 'youtube',
-                10: 'iphone',
-                11: 'ipad',
-                12: 'android',
-                13: 'steam',
-                14: 'Reddit',
+                1: { name: 'official', icon: 'fas fa-globe-americas' },
+                2: { name: 'wikia', icon: 'fas fa-link' },
+                3: { name: 'wikipedia', icon: 'fab fa-wikipedia-w' },
+                4: { name: 'facebook', icon: 'fab fa-facebook-square' },
+                5: { name: 'twitter', icon: 'fab fa-twitter' },
+                6: { name: 'twitch', icon: 'fab fa-twitch' },
+                8: { name: 'instagram', icon: 'fab fa-instagram' },
+                9: { name: 'youtube', icon: 'fab fa-youtube' },
+                10: { name: 'iphone', icon: 'fab fa-app-store-ios' },
+                11: { name: 'ipad' },
+                12: { name: 'android', icon: 'fab fa-google-play' },
+                13: { name: 'steam', icon: 'fab fa-steam' },
+                14: { name: 'reddit', icon: 'fab fa-reddit' },
+                15: { name: 'discord', icon: 'fab fa-discord' },
+                16: { name: 'google_plus', icon: 'fab fa-google' },
+                17: { name: 'tumblr', icon: 'fab fa-tumblr' },
+                18: { name: 'linkedin', icon: 'fab fa-linkedin' },
+                19: { name: 'pinterest', icon: 'fab fa-pinterest' },
+                20: { name: 'soundcloud', icon: 'fab fa-soundcloud' },
             },
         };
     },
@@ -69,15 +74,44 @@ export default {
 
 <style lang="scss" rel="stylesheet/scss" scoped>
     @import "~styles/styles.scss";
+
+    .game-links {
+        border-top: 1px solid $color-light-gray;
+        margin-top: $gp;
+        padding-top: $gp;
+    }
+
     .links {
-        display: flex;
-        flex-direction: column;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 40px);
         margin-top: $gp;
 
         a {
-            color: $color-blue;
-            text-transform: capitalize;
+            height: 40px;
+            font-size: 20px;
+            display: flex;
+            flex-direction: column;
+            color: $color-black;
             text-decoration: none;
+
+            &.twitter { color: #1da1f2; }
+            &.facebook { color: #3b5998; }
+            &.youtube { color: #c4302b; }
+            &.reddit { color: #ff4301; }
+            &.wikipedia { color: #636466; }
+            &.instagram {
+                background: -webkit-linear-gradient(
+                    #8a3ab9,
+                    #e95950,
+                    #bc2a8d,
+                    #fccc63,
+                    #fbad50,
+                    #cd486b
+                );
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+            &.steam { color: #000000; }
         }
     }
 </style>
