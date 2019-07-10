34d087aa5dde530d96b221407a4d8b894cb5d19c
Roman Cervantes <romancm@gmail.com>
2019-07-10 16:23:53 -0700

    Moved wallpaper upload to settings

--- src/components/GameBoard/GameBoardActions.vue
+++ src/components/GameBoard/GameBoardActions.vue
@@ -8,40 +8,6 @@
             >
                 <i class="fas fa-plus" />
             </button>
-
-            <modal
-                :title="$t('tags.title')"
-                ref="tags"
-                :message="$t('tags.useTags')"
-                padded
-            >
-                <button
-                    class="small accent"
-                    :title="$t('tags.title')"
-                >
-                    <i class="fas fa-tag" />
-                </button>
-
-                <tags slot="content" />
-            </modal>
-
-            <modal
-                :title="`${platform.name}`"
-                ref="settings"
-                padded
-            >
-                <button
-                    class="small accent"
-                    :title="$t('tags.title')"
-                >
-                    <i class="fas fa-cog" />
-                </button>
-
-                <game-board-settings
-                    slot="content"
-                    @update="update"
-                />
-            </modal>
         </div>
     </div>
 </template>
@@ -51,16 +17,12 @@ import { mapState } from 'vuex';
 import Modal from '@/components/Modal/Modal';
 import Tags from '@/components/Tags/Tags';
 import ListAdd from '@/components/Lists/ListAdd';
-import GameBoardSettings from '@/components/GameBoard/GameBoardSettings';
-import WallpaperUpload from '@/components/WallpaperUpload/WallpaperUpload';
 
 export default {
     components: {
         Modal,
         Tags,
-        WallpaperUpload,
         ListAdd,
-        GameBoardSettings,
     },
 
     computed: {
@@ -103,10 +65,5 @@ export default {
         display: block;
         padding: $gp / 2;
         border-radius: $border-radius;
-    }
-
-    .suggestions {
-        display: grid;
-        grid-gap: $gp;
     }
 </style>
