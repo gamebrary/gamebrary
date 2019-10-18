d284c811c3bffb8186d69a0816684774f773ff78
Roman Cervantes <romancm@gmail.com>
2019-10-18 10:27:05 -0700

    Game details clean up WIP

--- src/pages/GameDetail.vue
+++ src/pages/GameDetail.vue
@@ -3,83 +3,90 @@
         <game-detail-placeholder v-if="!game" :id="id" />
 
         <template v-else>
-            <div class="game-detail-container">
-                <pre>{{ tabs }}</pre>
-                <div class="checkbox-group">
-                    <span v-for="{ value } in tabs">
-                        <label for="videos" :class="{ active: value === tab }">
-                            <i class="fab fa-youtube" />
-                            Videos
-                        </label>
-                        <input type="radio" id="videos" :value="value" v-model="tab" />
-                    </span>
-                </div>
+            <header>
+                <aside>
+                    <img :src="coverUrl" :alt="game.name" class="game-cover" />
 
-                <!-- <component :is="gameCardComponent" /> -->
+                    <div class="game-rating" v-if="game.age_ratings">
+                        <img
+                        v-for="{ rating, synopsis, id } in game.age_ratings"
+                        :key="id"
+                        :src='`/static/img/age-ratings/${ageRatings[rating]}.png`'
+                        :alt="synopsis"
+                        />
+                    </div>
+                </aside>
 
-                <game-screenshots />
-                <game-videos />
-                <igdb-credit gray />
-                <div class="game-info">
-                    <div class="game-header">
-                        <img :src="coverUrl" :alt="game.name" class="game-cover" />
+                <article>
+                    <h2>{{ game.name }}</h2>
+                    {{ platform.name }}
+                    <game-rating :rating="game.rating" />
 
-                        <div class="game-rating" v-if="game.age_ratings">
-                            <img
-                                v-for="{ rating, synopsis, id } in game.age_ratings"
-                                :key="id"
-                                :src='`/static/img/age-ratings/${ageRatings[rating]}.png`'
-                                :alt="synopsis"
+                    <div class="actions">
+                        <button
+                            v-if="list.games.includes(game.id)"
+                            class="danger"
+                            @click="removeGame"
+                        >
+                            <i class="far fa-trash-alt delete-game" />
+                            {{ $t('gameDetail.removeFromList')}}
+                        </button>
+
+                        <button class="primary" v-else>
+                            {{ $t('list.addGame') }}
+
+                        </button>
+
+                        <div class="tags" v-if="hasTags">
+                            <button class="primary hollow" @click="openTags">
+                                <i class="fas fa-tag" />
+                                {{ $t('tags.addTag') }}
+                            </button>
+                        </div>
+                    </div>
+
+                    <p class="game-description" v-html="game.summary" />
+                    <game-notes />
+
+                    <section v-if="gamePlatforms && gamePlatforms.length > 0">
+                        <h4>{{ $t('gameDetail.gamePlatforms') }}</h4>
+
+                        <div class="platforms">
+                            <platform
+                                v-for="platform in gamePlatforms"
+                                :key="platform.name"
+                                :platform="platform"
                             />
                         </div>
+                    </section>
+
+                    <tag
+                        v-if="games.includes(game.id)"
+                        v-for="({ games, hex }, name) in tags"
+                        :key="name"
+                        :label="name"
+                        :hex="hex"
+                        readonly
+                        @action="openTags"
+                        @close="removeTag(name)"
+                    />
+
+                    <igdb-credit gray />
+
+                    <div class="tabs">
+                        <span v-for="{ value, icon, text } in tabs" :key="value">
+                            <label :for="value" :class="{ active: value === tab }">
+                                <i :class="icon" />
+                                {{ text }}
+                            </label>
+
+                            <input type="radio" :id="value" :value="value" v-model="tab" />
+                        </span>
                     </div>
 
-                    <div class="game-details">
-                        <h2>{{ game.name }}</h2>
-                        {{ platform.name }}
-                        <game-rating :rating="game.rating" />
-
-                        <div class="actions">
-                            <button
-                                v-if="list.games.includes(game.id)"
-                                class="danger"
-                                @click="removeGame"
-                            >
-                                <i class="far fa-trash-alt delete-game" />
-                                {{ $t('gameDetail.removeFromList')}}
-                            </button>
-
-                            <button class="primary" v-else>
-                                {{ $t('list.addGame') }}
-
-                            </button>
-
-                            <div class="tags" v-if="hasTags">
-                                <button class="primary hollow" @click="openTags">
-                                    <i class="fas fa-tag" />
-                                    {{ $t('tags.addTag') }}
-                                </button>
-                            </div>
-                        </div>
-
-                        <p class="game-description" v-html="game.summary" />
-
-                        <tag
-                            v-if="games.includes(game.id)"
-                            v-for="({ games, hex }, name) in tags"
-                            :key="name"
-                            :label="name"
-                            :hex="hex"
-                            readonly
-                            @action="openTags"
-                            @close="removeTag(name)"
-                        />
-
-                        <game-details />
-                        <game-notes />
-                    </div>
-                </div>
-            </div>
+                    <component :is="activeComponent" />
+                </article>
+            </header>
         </template>
     </div>
 </template>
@@ -87,12 +94,13 @@
 <script>
 import { mapState, mapGetters } from 'vuex';
 import Tag from '@/components/Tag';
-// import GameHeader from '@/components/GameDetail/GameHeader';
 import GameScreenshots from '@/components/GameDetail/GameScreenshots';
 import GameNotes from '@/components/GameNotes';
 import GameRating from '@/components/GameDetail/GameRating';
+import GameLinks from '@/components/GameDetail/GameLinks';
 import GameVideos from '@/components/GameDetail/GameVideos';
 import GameDetails from '@/components/GameDetail/GameDetails';
+import Platform from '@/components/Platforms/Platform';
 import IgdbCredit from '@/components/IgdbCredit';
 import GameDetailPlaceholder from '@/components/GameDetail/GameDetailPlaceholder';
 import firebase from 'firebase/app';
@@ -104,27 +112,14 @@ export default {
     components: {
         IgdbCredit,
         Tag,
-        // GameHeader,
         GameRating,
+        GameLinks,
         GameScreenshots,
         GameNotes,
+        Platform,
         GameVideos,
         GameDetails,
         GameDetailPlaceholder,
-    },
-
-    data() {
-        return {
-            tab: '',
-            tabs: [
-                {
-                    value: 'videos',
-                    icon: 'fab fa-youtube',
-                    text: 'Videos',
-                    component: 'GameVideos',
-                }
-            ]
-        };
     },
 
     props: {
@@ -132,12 +127,52 @@ export default {
         listId: [Number, String],
     },
 
+    data() {
+        return {
+            tab: 'details',
+            tabs: [
+                {
+                    value: 'details',
+                    icon: 'fab fa-youtube',
+                    text: 'Details',
+                    component: 'GameDetails',
+                },
+                {
+                    value: 'videos',
+                    icon: 'fab fa-youtube',
+                    text: 'Videos',
+                    component: 'GameVideos',
+                },
+                {
+                    value: 'screenshots',
+                    icon: 'fas fa-images',
+                    text: 'Screenshots',
+                    component: 'GameScreenshots',
+                },
+                {
+                    value: 'links',
+                    icon: 'fas fa-link',
+                    text: 'Links',
+                    component: 'GameLinks',
+                },
+            ],
+        };
+    },
+
     computed: {
         ...mapState(['game', 'user', 'platform', 'tags', 'gameLists']),
-        ...mapGetters(['ageRatings']),
+        ...mapGetters(['ageRatings', 'gamePlatforms']),
 
         hasTags() {
             return Object.keys(this.tags) && Object.keys(this.tags).length > 0;
+        },
+
+        activeComponent() {
+            const activeTab = this.tabs.find(tab => tab.value === this.tab);
+
+            return activeTab
+                ? activeTab.component
+                : '';
         },
 
         activePlatform() {
@@ -218,5 +253,44 @@ export default {
 <style lang="scss" rel="stylesheet/scss" scoped>
     @import "~styles/styles";
 
+    header {
+        display: grid;
+        grid-template-columns: 200px auto;
+        margin-top: $gp;
 
+        @media($small) {
+            grid-template-columns: auto;
+        }
+    }
+
+    aside {
+        @media($small) {
+            text-align: center;
+        }
+    }
+
+    .game-cover {
+        border-radius: $border-radius;
+    }
+
+    .platforms {
+        margin-top: $gp;
+        display: flex;
+        flex-wrap: wrap;
+
+        .platform {
+            width: 90px;
+            height: 50px;
+            margin-right: $gp / 2;
+            padding: $gp / 4;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &:last-child {
+                margin-right: 0;
+            }
+        }
+    }
 </style>
