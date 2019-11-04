7d70ad702802b4c1b1843dfb85bb7568ec0d3616
Roman Cervantes <romancm@gmail.com>
2019-11-04 14:26:19 -0700

    Save list without merge action

--- src/components/Settings/GameBoardSettings.vue
+++ src/components/Settings/GameBoardSettings.vue
@@ -116,10 +116,7 @@ export default {
         deletePlatform() {
             this.$store.commit('REMOVE_PLATFORM');
 
-            const db = firebase.firestore();
-
-            // TODO: move to actions
-            db.collection('lists').doc(this.user.uid).set(this.gameLists, { merge: false })
+            this.$store.dispatch('SAVE_LIST_NO_MERGE', this.gameLists)
                 .then(() => {
                     this.$router.push({ name: 'platforms' });
                 })
