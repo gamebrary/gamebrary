4c61c2708a6e07a27b3a947e064b2a7527eb1484
Gamebrary <contact@gamebrary.com>
2020-08-14 16:57:40 -0700

    update and refactor igdb logo component

---
+++ src/components/IgdbLogo.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <b-link
+    href="https://www.igdb.com/"
+    class="igdb-credit m-0 d-flex text-center text-decoration-none"
+    target="_blank"
+  >
+    <small class="text-muted pr-1">{{ $t('poweredBy') }}</small>
+
+    <b-img
+      center
+      width="40"
+      :src="`/static/img/igdb-logo.png`"
+      :alt="$t('poweredByIgdb')"
+    />
+  </b-link>
+</template>
