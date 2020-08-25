a84be79a9adfdf4f791b3a71ba9f14d33cccc537
Gamebrary <contact@gamebrary.com>
2020-08-24 21:21:56 -0700

    filtered platforms getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,6 @@
+import { PLATFORM_FILTER_FIELDS } from '@/constants';
 import platforms from '@/platforms';
+import orderby from 'lodash.orderby';
 
 export default {
   gamePlatforms: (state) => {
@@ -7,6 +9,29 @@ export default {
       : null;
 
     return platforms.filter(({ id }) => id !== state.platform.id && gamePlatforms.includes(id));
+  },
+
+  filteredPlatforms: (state) => {
+    const filterField = state.settings.platformsFilterField || null;
+    const sortField = state.settings.platformsSortField || 'generation';
+
+    let filteredPlatforms = [];
+
+    if (filterField === 'popular') {
+      filteredPlatforms = state.platforms.filter(({ popular }) => popular);
+    } else {
+      filteredPlatforms = filterField && PLATFORM_FILTER_FIELDS.includes(filterField)
+        ? state.platforms.filter(({ category }) => category === filterField)
+        : state.platforms;
+    }
+
+    const sortedPlatforms = orderby(filteredPlatforms, sortField);
+
+    const reverseOrder = ['releaseYear', 'generation'].includes(sortField);
+
+    return reverseOrder
+      ? sortedPlatforms.reverse()
+      : sortedPlatforms;
   },
 
   // eslint-disable-next-line
