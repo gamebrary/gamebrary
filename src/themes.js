41a081006db9def05d89f0d390553b398433e288
Gamebrary <contact@gamebrary.com>
2020-08-25 22:01:48 -0700

    clean up themes

--- src/themes.js
+++ src/themes.js
@@ -9,54 +9,9 @@ export default [
     },
   },
   {
-    name: 'City lights',
-    id: 'city-lights',
-    description: 'Inspired on the awesome theme developed by YummyGum http://citylights.xyz/',
-    author: {
-      name: 'Gamebrary staff',
-      url: 'https://gamebrary.com',
-    },
-  },
-  {
     name: 'Dark',
     id: 'dark',
     description: 'Dark theme',
-    author: {
-      name: 'Gamebrary staff',
-      url: 'https://gamebrary.com',
-    },
-  },
-  {
-    name: 'Tomorrow Night',
-    id: 'tomorrow-night',
-    description: 'Tomorrow Night theme',
-    author: {
-      name: 'Chris Kempson',
-      url: 'https://github.com/chriskempson/tomorrow-theme',
-    },
-  },
-  {
-    name: 'Tomorrow Night Bright',
-    id: 'tomorrow-night-bright',
-    description: 'Tomorrow Night Bright theme',
-    author: {
-      name: 'Chris Kempson',
-      url: 'https://github.com/chriskempson/tomorrow-theme',
-    },
-  },
-  {
-    name: 'Blue',
-    id: 'blue',
-    description: 'Blue theme',
-    author: {
-      name: 'Gamebrary staff',
-      url: 'https://gamebrary.com',
-    },
-  },
-  {
-    name: 'Hyrule',
-    id: 'hyrule',
-    description: 'Theme inspired by Hyrule colors',
     author: {
       name: 'Gamebrary staff',
       url: 'https://gamebrary.com',
