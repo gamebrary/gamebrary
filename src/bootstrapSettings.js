f239dc5297359f8ea4baaa567dd0b5e08874e530
Gamebrary <contact@gamebrary.com>
2020-10-08 13:45:33 -0400

    update close icon and add reference to settings

--- src/bootstrapSettings.js
+++ src/bootstrapSettings.js
@@ -1,9 +1,11 @@
+// https://bootstrap-vue.org/docs/reference/settings
+
 export default {
   BToast: {
     toaster: 'b-toaster-bottom-right',
     autoHideDelay: 1000,
   },
   BModal: {
-    headerCloseContent: '<svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="x" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-x b-icon bi"><g><path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"></path><path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"></path></g></svg>',
+    headerCloseContent: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"/></svg>',
   },
 };
