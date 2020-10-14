5de1f7dc45413dcfcc83395ab9b35b3bc701c4f7
Gamebrary <contact@gamebrary.com>
2020-10-13 17:11:08 -0700

    Improved empty board

---
+++ src/components/Board/EmptyBoard.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <b-jumbotron class="empty-board w-100 text-center">
+    <p>This board is empty</p>
+    <b-button variant="primary" @click="openAddListModal">
+      Add list
+    </b-button>
+  </b-jumbotron>
+</template>
+
+<script>
+export default {
+  methods: {
+    openAddListModal() {
+      this.$bvModal.show('add-list');
+    },
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+.empty-board {
+  max-width: 300px;
+}
+</style>
