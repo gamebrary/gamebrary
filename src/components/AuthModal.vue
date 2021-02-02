7e2fd62dfd820712e4abe85e3e5f67b6d51b74d1
Gamebrary <contact@gamebrary.com>
2021-02-02 16:32:30 -0700

    Show authentication modal for public profiles

---
+++ src/components/AuthModal.vue
@@ -0,0 +1,59 @@
+<template lang="html">
+  <b-modal
+    id="authModal"
+    hide-footer
+    centered
+  >
+    <template v-slot:modal-header="{ close }">
+      <modal-header
+        title="Gamebrary"
+        subtitle="Ditch the spreadsheet. Organize your games with Gamebrary!"
+        @close="close"
+      >
+        <template v-slot:header>
+          <img
+            class="logo mr-2"
+            :src="`/static/gamebrary-logo${nightMode ? '' : '-dark'}.png`"
+          />
+        </template>
+      </modal-header>
+    </template>
+
+    <div class="text-center">
+      <b-img
+        src="/static/img/screenshot-1.jpg"
+        class="rounded shadow w-75 ml-auto mr-auto mt-2 mb-3"
+        block
+      />
+
+      <h2>Hi 👋!</h2>
+
+      <p>Gamebrary is an open source tool to organize video game collections.</p>
+
+      <b-button
+        :to="{ name: 'auth' }"
+        variant="primary"
+      >
+        <i class="fab fa-google fa-fw" aria-hidden />
+        Authenticate with Google
+      </b-button>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['nightMode']),
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+.logo {
+  float: left;
+  height: 40px;
+}
+</style>
