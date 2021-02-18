2cd19bfc8e280c0ce613c89ef98e171084b2d37e
Gamebrary <contact@gamebrary.com>
2021-02-17 17:50:37 -0700

    wallpapers list component

---
+++ src/components/WallpapersList.vue
@@ -0,0 +1,126 @@
+<template lang="html">
+  <div :class="['wallpapers', { page }]">
+    <b-card
+      v-for="wallpaper in wallpapers"
+      :key="wallpaper.name"
+      :img-src="wallpaper.url"
+      :img-alt="wallpaper.name"
+      img-top
+      bg-variant="transparent"
+      img-width="180"
+      class="mb-3 overflow-hidden word-wrap"
+    >
+      <h6>
+        {{ wallpaper.name }}
+
+        <b-badge v-if="wallpaper.metadata && wallpaper.metadata.size">
+          {{ bytesToSize(wallpaper.metadata.size) }}
+        </b-badge>
+      </h6>
+
+      <b-button
+        variant="danger"
+        @click="confirmDeleteWallpaper(wallpaper)"
+      >
+        <i class="fas fa-trash-alt fa-fw" aria-hidden />
+      </b-button>
+
+      <b-button
+        v-if="selectable"
+        variant="primary"
+        @click="handleClick(wallpaper)"
+      >
+        <i
+          v-if="saving"
+          class="fas fa-sync fa-spin fa-fw"
+          aria-hidden
+        />
+
+        <span v-else>Select</span>
+      </b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  props: {
+    selectable: Boolean,
+    page: Boolean,
+    saving: Boolean,
+  },
+
+  computed: {
+    ...mapState(['wallpapers']),
+  },
+
+  methods: {
+    handleClick(wallpaper) {
+      if (this.selectable) {
+        this.$emit('selected', wallpaper);
+      }
+    },
+
+    bytesToSize(bytes) {
+      // TODO: put in util
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+      if (bytes === 0) return '0 Byte';
+
+      const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 0);
+
+      return `${Math.round(bytes / (1024 ** i), 2)} ${sizes[i]}`;
+    },
+
+    confirmDeleteWallpaper(file) {
+      this.$bvModal.msgBoxConfirm('Wallpaper will be permanently removed', {
+        title: 'Are you sure you want to delete this file?',
+        okVariant: 'danger',
+        okTitle: 'Yes',
+      })
+        .then((value) => {
+          if (value) {
+            this.deleteFile(file);
+          }
+        });
+    },
+
+    async deleteFile(file) {
+      await this.$store.dispatch('DELETE_WALLPAPER', file)
+        .catch(() => {
+          this.$bvToast.toast('There was an error deleting wallpaper', { variant: 'danger' });
+        });
+
+      this.$bvToast.toast('File deleted');
+
+      // const { board } = this;
+      // TODO: handle wallpapers in use
+      // if (board.wallpaper && this.board.wallpaper === file.path) {
+      //   this.$bus.$emit('RELOAD_WALLPAPER');
+      // }
+    },
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+.wallpapers {
+  display: grid;
+  grid-column-gap: 1rem;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  &.page {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+
+    @media(max-width: 1024px) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+
+  @media(max-width: 720px) {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+</style>
--- src/pages/Wallpapers.vue
+++ src/pages/Wallpapers.vue
@@ -16,9 +16,11 @@
         </h3>
 
         <div class="space-used ml-auto mr-3 pt-3">
-          <small class="d-block text-center" :class="{ 'text-danger': outOfSpace }">
-            {{ formattedSpaceUsed }} of {{ bytesToSize(maxSpace) }} used
-          </small>
+          <small
+            class="d-block text-center"
+            :class="{ 'text-danger': outOfSpace }"
+            v-text="usedSpaceText"
+          />
 
           <b-progress
             :value="spaceUsed"
@@ -51,37 +53,7 @@
         {{ $t('wallpapers.form.duplicateMessage', { fileName: file.name }) }}
       </b-alert>
 
-      <div
-        v-if="wallpapers.length"
-        class="wallpapers"
-      >
-        <b-card
-          v-for="wallpaper in wallpapers"
-          :key="wallpaper.name"
-          :img-src="wallpaper.url"
-          :img-alt="wallpaper.name"
-          img-top
-          bg-variant="transparent"
-          img-width="180"
-          class="mb-3 overflow-hidden word-wrap"
-        >
-          <h6>
-            {{ wallpaper.name }}
-
-            <b-badge v-if="wallpaper.metadata && wallpaper.metadata.size">
-              {{ bytesToSize(wallpaper.metadata.size) }}
-            </b-badge>
-          </h6>
-
-          <b-button
-            variant="danger"
-            size="sm"
-            @click="confirmDeleteWallpaper(wallpaper)"
-          >
-            <i class="fas fa-trash-alt fa-fw" aria-hidden />
-          </b-button>
-        </b-card>
-      </div>
+      <wallpapers-list page v-if="wallpapers.length" />
 
       <b-alert show v-else>You don't have any wallpapers.</b-alert>
     </template>
@@ -100,10 +72,13 @@
 <script>
 import { mapState, mapGetters } from 'vuex';
 import EmptyState from '@/components/EmptyState';
+import { bytesToSize } from '@/utils';
+import WallpapersList from '@/components/WallpapersList';
 
 export default {
   components: {
     EmptyState,
+    WallpapersList,
   },
 
   data() {
@@ -121,6 +96,10 @@ export default {
     ...mapState(['user', 'board', 'wallpapers']),
     ...mapGetters(['nightMode']),
 
+    usedSpaceText() {
+      return `${this.formattedSpaceUsed} of ${bytesToSize(this.maxSpace)} used`;
+    },
+
     existingFiles() {
       return this.wallpapers.map(({ name }) => name);
     },
@@ -137,8 +116,8 @@ export default {
 
     formattedSpaceUsed() {
       return this.spaceUsed
-        ? this.bytesToSize(this.spaceUsed)
-        : null;
+        ? bytesToSize(this.spaceUsed)
+        : 0;
     },
 
     spaceUsed() {
@@ -153,16 +132,6 @@ export default {
   methods: {
     triggerFileUpload() {
       document.querySelector('.file-input input').click();
-    },
-
-    bytesToSize(bytes) {
-      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-
-      if (bytes === 0) return '0 Byte';
-
-      const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 0);
-
-      return `${Math.round(bytes / (1024 ** i), 2)} ${sizes[i]}`;
     },
 
     uploadWallpaper() {
@@ -188,59 +157,12 @@ export default {
           this.$bvToast.toast('There was an error uploading wallpaper', { variant: 'danger' });
         });
     },
-
-    confirmDeleteWallpaper(file) {
-      this.$bvModal.msgBoxConfirm('Wallpaper will be permanently removed', {
-        title: 'Are you sure you want to delete this file?',
-        okVariant: 'danger',
-        okTitle: 'Yes',
-      })
-        .then((value) => {
-          if (value) {
-            this.deleteFile(file);
-          }
-        });
-    },
-
-    async deleteFile(file) {
-      await this.$store.dispatch('DELETE_WALLPAPER', file)
-        .catch(() => {
-          this.$bvToast.toast('There was an error deleting wallpaper', { variant: 'danger' });
-        });
-
-      this.$bvToast.toast('File deleted');
-
-      // const { board } = this;
-      // TODO: handle wallpapers in use
-      // if (board.wallpaper && this.board.wallpaper === file.path) {
-      //   this.$bus.$emit('RELOAD_WALLPAPER');
-      // }
-    },
   },
 };
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-.delete-file {
-  bottom: .5rem;
-  right: .5rem;
-}
-
 .space-used {
   width: 120px;
-}
-
-.wallpapers {
-  display: grid;
-  grid-column-gap: 1rem;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-
-  @media(max-width: 1024px) {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-
-  @media(max-width: 720px) {
-    grid-template-columns: 1fr 1fr;
-  }
 }
 </style>
