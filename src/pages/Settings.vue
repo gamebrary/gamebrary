4b2e1ca4b82fafe837536a3c782eb3370976897b
Gamebrary <contact@gamebrary.com>
2021-02-21 12:08:22 -0700

    update settings layout to include profile

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,56 +1,61 @@
 <template lang="html">
   <b-container>
-    <div class="d-flex justify-content-between align-items-center mb-3">
-      <h3 class="m-0">Settings</h3>
-    </div>
+    <b-row>
+      <b-col cols="12" sm="6" class="mt-4 mt-sm-0 mb-3">
+        <b-list-group>
+          <b-list-group-item :variant="darkTheme ? 'info' : null">
+            <strong>{{ user.displayName }}</strong>
+            <p class="text-muted m-0">{{ user.email }}</p>
+          </b-list-group-item>
 
-    <languages />
-    <theme-selector />
+          <b-list-group-item :variant="darkTheme ? 'dark' : null">
+            <strong>Date Joined</strong>
+            <p class="text-muted m-0">{{ formatDate(user.dateJoined) }}</p>
+          </b-list-group-item>
 
-    <b-list-group>
-      <b-list-group-item :variant="darkTheme ? 'dark' : null">
-        <strong>{{ user.displayName }}</strong>
-        <p class="text-muted m-0">{{ user.email }}</p>
-      </b-list-group-item>
+          <b-list-group-item :variant="darkTheme ? 'info' : null">
+            <strong>Last login</strong>
+            <p class="text-muted m-0">{{ formatDate(user.lastLogin) }}</p>
+          </b-list-group-item>
 
-      <b-list-group-item :variant="darkTheme ? 'dark' : null">
-        <strong>Date Joined</strong>
-        <p class="text-muted m-0">{{ formatDate(user.dateJoined) }}</p>
-      </b-list-group-item>
+          <b-list-group-item :variant="darkTheme ? 'dark' : null">
+            <strong>Email Verified</strong>
+            <p class="text-muted m-0">{{ user.emailVerified ? 'Yes' : 'No' }}</p>
+          </b-list-group-item>
 
-      <b-list-group-item :variant="darkTheme ? 'dark' : null">
-        <strong>Last login</strong>
-        <p class="text-muted m-0">{{ formatDate(user.lastLogin) }}</p>
-      </b-list-group-item>
+          <b-list-group-item :variant="darkTheme ? 'info' : null">
+            <strong>ID</strong>
+            <p class="text-muted m-0 mb-2">{{ user.uid }}</p>
 
-      <b-list-group-item :variant="darkTheme ? 'dark' : null">
-        <strong>Email Verified</strong>
-        <p class="text-muted m-0">{{ user.emailVerified ? 'Yes' : 'No' }}</p>
-      </b-list-group-item>
+            <b-alert show>You can safely share this id when reporting bugs</b-alert>
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
 
-      <b-list-group-item :variant="darkTheme ? 'dark' : null">
-        <strong>ID</strong>
-        <p class="text-muted m-0 mb-2">{{ user.uid }}</p>
+      <b-col cols="12" sm="6">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+          <h3 class="m-0">Settings</h3>
+        </div>
 
-        <b-alert show>You can safely share this id when reporting bugs</b-alert>
-      </b-list-group-item>
-    </b-list-group>
+        <div class="d-inline-flex flex-column align-items-start">
+          <languages />
 
-    <hr />
+          <theme-selector class="mb-2" />
 
-    <b-button
-      href="https://accounts.google.com/"
-      class="mb-2"
-      target="_blank"
-      variant="info"
-    >
-      Manage Google Account
-    </b-button>
+          <b-button
+            href="https://accounts.google.com/"
+            class="mb-2 mt-4"
+            target="_blank"
+            variant="info"
+          >
+            Manage Google Account
+          </b-button>
 
-    <br />
-    <delete-account class="mb-2" />
-    <br />
-    <sign-out />
+          <sign-out class="mb-2" />
+          <delete-account />
+        </div>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -59,7 +64,7 @@ import Languages from '@/components/Settings/Languages';
 import ThemeSelector from '@/components/Settings/ThemeSelector';
 import SignOut from '@/components/Settings/SignOut';
 import DeleteAccount from '@/components/Settings/DeleteAccount';
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   components: {
@@ -71,6 +76,7 @@ export default {
 
   computed: {
     ...mapState(['settings', 'user']),
+    ...mapGetters(['darkTheme']),
   },
 
   methods: {
