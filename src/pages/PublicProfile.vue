e67eb896f2b9a852c4ad369bc09e8b44eb0c3604
Gamebrary <contact@gamebrary.com>
2021-03-29 16:13:02 -0700

    remove silly text

--- src/pages/PublicProfile.vue
+++ src/pages/PublicProfile.vue
@@ -1,13 +1,4 @@
 <template lang="html">
   <div>
-    zis is public profile
   </div>
 </template>
-
-<script>
-export default {
-};
-</script>
-
-<style lang="scss" rel="stylesheet/scss" scoped>
-</style>
