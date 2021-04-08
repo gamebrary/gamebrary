d637eeb541b6e80cd13c1b054f576b46f4cc1baa
Gamebrary <contact@gamebrary.com>
2021-04-08 16:19:49 -0700

    Show which provider is used in settings

---
+++ src/components/ProviderCard.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div v-if="provider" class="d-flex flex-column border rounded bg-white py-2 px-3">
+    <p class="lead mb-0">{{ user.displayName }}</p>
+    <small>Member since {{ dateJoined }}</small>
+    <small>Last login {{ lastLogin }}</small>
+
+    <small class="text-muted mt-2 pt-2 border-top">
+      Logged in with <strong>{{ provider.name }}</strong>
+    </small>
+  </div>
+</template>
+
+<script>
+import { AUTH_PROVIDERS } from '@/constants';
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['user']),
+
+    provider() {
+      return AUTH_PROVIDERS[this.user.providerId];
+    },
+
+    dateJoined() {
+      return this.user && this.user.dateJoined
+        ? this.$dayjs(this.user.dateJoined).format('M/D/YYYY')
+        : null;
+    },
+
+    lastLogin() {
+      return this.user && this.user.lastLogin
+        ? this.$dayjs(this.user.lastLogin).format('M/D/YYYY')
+        : null;
+    },
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+</style>
--- src/pages/Preferences.vue
+++ src/pages/Preferences.vue
@@ -1,28 +1,18 @@
 <template lang="html">
   <div>
     <div class="d-inline-flex flex-column align-items-start">
-      <b-alert show variant="warning" v-if="!user.emailVerified" class="mb-2">
-        Your email hasn't been verified.
-      </b-alert>
-
       <languages />
-
       <theme-selector class="mb-2" />
 
-      <b-button
-        v-if="user.providerId === 'google.com'"
-        href="https://accounts.google.com/"
-        class="mb-2 mt-4"
-        target="_blank"
-        variant="info"
-      >
-        Manage Google Account
-      </b-button>
+      <div class="border-top w-100 mt-3 mb-4" />
+
+      <provider-card />
 
       <b-button
         @click="session_signOut"
-        variant="info"
-        class="mb-2"
+        variant="outline-info"
+        size="sm"
+        class="my-2"
       >
         {{ $t('global.signOut') }}
       </b-button>
@@ -35,6 +25,7 @@
 <script>
 import Languages from '@/components/Settings/Languages';
 import ThemeSelector from '@/components/Settings/ThemeSelector';
+import ProviderCard from '@/components/ProviderCard';
 import DeleteAccount from '@/components/Settings/DeleteAccount';
 import SessionMixin from '@/mixins/SessionMixin';
 import { mapState } from 'vuex';
@@ -43,6 +34,7 @@ export default {
   components: {
     Languages,
     ThemeSelector,
+    ProviderCard,
     DeleteAccount,
   },
 
