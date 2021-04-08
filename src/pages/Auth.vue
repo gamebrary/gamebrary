658d660ea17ae4a18ce0dc579ca6def1ad1b648d
Gamebrary <contact@gamebrary.com>
2021-04-08 15:05:34 -0700

    remove facebook because facebook sucks.

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -77,7 +77,6 @@ export default {
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
           firebase.auth.EmailAuthProvider.PROVIDER_ID,
         ],
         tosUrl: 'https://gamebrary.com/terms/',
