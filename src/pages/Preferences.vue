322817f40f48c44e70a2823edd6fe2bda9b617bc
Gamebrary <contact@gamebrary.com>
2021-04-14 16:31:49 -0700

    preferences placeholders

--- src/pages/Preferences.vue
+++ src/pages/Preferences.vue
@@ -4,6 +4,22 @@
       <languages />
       <theme-selector class="mb-2" />
 
+      <br>
+
+      Open games in:
+      <b-form-select v-model="gameDetailView" class="mb-3">
+        <b-form-select-option value="modal">Modal</b-form-select-option>
+        <b-form-select-option value="side">Side panel</b-form-select-option>
+        <b-form-select-option value="new">New page</b-form-select-option>
+      </b-form-select>
+
+      Dock position:
+      <b-form-select v-model="gameDetailView" class="mb-3">
+        <b-form-select-option value="top">Top</b-form-select-option>
+        <b-form-select-option value="left">Left</b-form-select-option>
+        <b-form-select-option value="bottom">Bottom</b-form-select-option>
+      </b-form-select>
+
       <div class="border-top w-100 mt-3 mb-4" />
 
       <provider-card />
