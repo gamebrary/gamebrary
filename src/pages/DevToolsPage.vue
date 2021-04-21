f1bcb65a5bc8f8a8a437a12200a4b3b696670e4f
Gamebrary <contact@gamebrary.com>
2021-04-21 08:40:27 -0700

    dev tools page

---
+++ src/pages/DevToolsPage.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <b-container>
+    <page-title
+      title="Dev tools"
+    />
+
+    <dev-tools />
+  </b-container>
+</template>
+
+<script>
+import DevTools from '@/components/DevTools';
+
+export default {
+  components: {
+    DevTools,
+  },
+};
+</script>
