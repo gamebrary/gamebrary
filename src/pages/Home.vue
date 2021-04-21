35aa5b598b08e50bdec3070c56f52474bfef84ba
Gamebrary <contact@gamebrary.com>
2021-04-21 08:40:06 -0700

    home page placeholder

---
+++ src/pages/Home.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <b-container v-if="user">
+    <!-- TODO: show more stuff, public boards -->
+    <page-title
+      title="Boards"
+      action-text="Create board"
+      @action="$bvModal.show('create-board')"
+    />
+
+    <boards />
+    <!-- public boards -->
+    <!-- news feeds -->
+    <!-- deals -->
+    <!-- newsletter signup -->
+  </b-container>
+</template>
+
+<script>
+import Boards from '@/components/Boards';
+import { mapState } from 'vuex';
+
+export default {
+  components: {
+    Boards,
+  },
+
+  computed: {
+    ...mapState(['user']),
+  },
+};
+</script>
