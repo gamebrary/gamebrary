86b8e482de5891277b86f5bf0bf1714e602fd5d4
Gamebrary <contact@gamebrary.com>
2021-04-20 22:58:50 -0700

    dev tools

---
+++ src/components/DevTools.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <div content-class="mt-3">
+    <div
+      v-for="variant in variants"
+      :key="variant"
+      class="mb-3"
+    >
+      <b-button
+        v-for="size in ['sm', '', 'lg']"
+        :variant="variant"
+        :key="size"
+        :size="size"
+        class="mr-2"
+      >
+        {{ variant }}
+      </b-button>
+    </div>
+
+    <b-dropdown text="Dropdown Button">
+      <b-dropdown-item>First Action</b-dropdown-item>
+      <b-dropdown-item>Second Action</b-dropdown-item>
+      <b-dropdown-item>Third Action</b-dropdown-item>
+      <b-dropdown-divider></b-dropdown-divider>
+      <b-dropdown-item active>Active action</b-dropdown-item>
+      <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+    </b-dropdown>
+
+    <b-button
+      @click="$store.commit('SET_SESSION_EXPIRED', true)"
+      variant="warning"
+    >
+      Expire session
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      variants: [
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+        'link',
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+</style>
--- src/components/DevToolsModal.vue
+++ src/components/DevToolsModal.vue
@@ -9,67 +9,17 @@
     </template>
 
     <!-- TODO: add page version as well -->
-    <b-tabs content-class="mt-3">
-      <b-tab title="Buttons">
-        <div
-          v-for="variant in variants"
-          :key="variant"
-          class="mb-3"
-        >
-          <b-button
-            v-for="size in ['sm', '', 'lg']"
-            :variant="variant"
-            :key="size"
-            :size="size"
-            class="mr-2"
-          >
-            {{ variant }}
-          </b-button>
-        </div>
-      </b-tab>
-
-      <b-tab title="Session">
-        <b-button @click="$store.commit('SET_SESSION_EXPIRED', true)">
-          Expire session
-        </b-button>
-      </b-tab>
-    </b-tabs>
+    <dev-tools />
 
   </b-modal>
 </template>
 
 <script>
+import DevTools from '@/components/DevTools';
+
 export default {
-  data() {
-    return {
-      open: true,
-      variants: [
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-        'link',
-      ],
-    };
-  },
-
-  methods: {
-    login() {
-      this.$store.commit('CLEAR_SESSION');
-      this.$store.commit('SET_SESSION_EXPIRED', false);
-      window.location.href = this.session_authUrl;
-    },
-
-    logout() {
-      this.$store.commit('CLEAR_SESSION');
-      this.$store.commit('SET_SESSION_EXPIRED', false);
-
-      window.location.href = this.session_publicSiteUrl;
-    },
+  components: {
+    DevTools,
   },
 };
 </script>
