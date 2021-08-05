bacccbb465ae95dba55f4f7f00cf0dc359f7049d
Gamebrary <contact@gamebrary.com>
2021-08-04 22:21:33 -0700

    dashboard placeholder

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <b-container fluid>
+    Dashboard
+    <page-title
+      title="Boards"
+      action-text="Create board"
+      @action="$bvModal.show('create-board')"
+    />
+
+    <h5>public boards</h5>
+    <h5>news feeds</h5>
+    <h5>deals</h5>
+    <h5>newsletter signup</h5>
+  </b-container>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+</style>
