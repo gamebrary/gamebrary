f4819938e66929bcc669146d1083718cf6c666d3
Gamebrary <contact@gamebrary.com>
2021-12-19 21:47:39 -0700

    improve settings page layout

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -2,46 +2,52 @@
   <b-container fluid>
     <page-title title="Settings" />
 
-    <b-list-group>
-      <b-list-group-item :to="{ name: 'general-settings' }">
-        <i class="fas fa-sliders-h fa-fw" aria-hidden />
-        General
-      </b-list-group-item>
+    <b-row no-gutters>
+      <b-col cols="2">
+        <b-list-group>
+          <b-list-group-item :to="{ name: 'general-settings' }">
+            <i class="fas fa-sliders-h fa-fw" aria-hidden />
+            General
+          </b-list-group-item>
 
-      <b-list-group-item :to="{ name: 'wallpapers-settings' }">
-        <i class="fas fa-sliders-h fa-fw" aria-hidden />
-        Wallpapers
-      </b-list-group-item>
+          <b-list-group-item :to="{ name: 'wallpapers-settings' }">
+            <i class="fas fa-sliders-h fa-fw" aria-hidden />
+            Wallpapers
+          </b-list-group-item>
 
-      <b-list-group-item :to="{ name: 'profile-settings' }">
-        <i class="fas fa-sliders-h fa-fw" aria-hidden />
-        Profile
-      </b-list-group-item>
+          <b-list-group-item :to="{ name: 'profile-settings' }">
+            <i class="fas fa-sliders-h fa-fw" aria-hidden />
+            Profile
+          </b-list-group-item>
 
-      <b-list-group-item :to="{ name: 'notes-settings' }">
-        <i class="fas fa-sliders-h fa-fw" aria-hidden />
-        Notes
-      </b-list-group-item>
+          <b-list-group-item :to="{ name: 'notes-settings' }">
+            <i class="fas fa-sliders-h fa-fw" aria-hidden />
+            Notes
+          </b-list-group-item>
 
-      <b-list-group-item :to="{ name: 'tags-settings' }">
-        <i class="fas fa-sliders-h fa-fw" aria-hidden />
-        Tags
-      </b-list-group-item>
+          <b-list-group-item :to="{ name: 'tags-settings' }">
+            <i class="fas fa-sliders-h fa-fw" aria-hidden />
+            Tags
+          </b-list-group-item>
 
-      <b-list-group-item :to="{ name: 'account-settings' }">
-        <i class="fas fa-sliders-h fa-fw" aria-hidden />
-        Account
-      </b-list-group-item>
+          <b-list-group-item :to="{ name: 'account-settings' }">
+            <i class="fas fa-sliders-h fa-fw" aria-hidden />
+            Account
+          </b-list-group-item>
 
-      <b-list-group-item :to="{ name: 'steam-settings' }">
-        <i class="fab fa-steam fa-fw" aria-hidden></i>
-        Steam
-      </b-list-group-item>
-    </b-list-group>
+          <b-list-group-item :to="{ name: 'steam-settings' }">
+            <i class="fab fa-steam fa-fw" aria-hidden></i>
+            Steam
+          </b-list-group-item>
 
-    <router-view />
+          <a href="#">Keyboard shortcuts</a>
+          <a href="#">Releases</a>
+        </b-list-group>
+      </b-col>
 
-    <a href="#">Keyboard shortcuts</a>
-    <a href="#">Releases</a>
+      <b-col>
+        <router-view />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
