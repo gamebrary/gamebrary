e4d8c33fe346d7ead266c67c337be24505d5a9e4
Gamebrary <contact@gamebrary.com>
2022-03-18 09:31:54 -0700

    put steam base url in constant

--- functions/steam.js
+++ functions/steam.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
-// const BASE_URL =
-// TODO: put base url in constant
+
+const STEAM_BASE_URL = 'https://api.steampowered.com';
 
 exports.game = functions.https.onRequest((req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
@@ -29,7 +29,7 @@ exports.news = functions.https.onRequest((req, res) => {
   if (!appId) res.send(400);
 
   axios({
-    url: `https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${appId}`,
+    url: `${STEAM_BASE_URL}/ISteamNews/GetNewsForApp/v0002/?appid=${appId}`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -47,7 +47,7 @@ exports.friends = functions.https.onRequest((req, res) => {
   if (!appId) res.send(400);
 
   axios({
-    url: 'https://api.steampowered.com/ISteamUser/GetFriendList/v1?key=0DFD0511A922896D8064A4EABC32BE08&steamid=76561198017003200',
+    url: `${STEAM_BASE_URL}/ISteamUser/GetFriendList/v1?key=0DFD0511A922896D8064A4EABC32BE08&steamid=76561198017003200`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -65,7 +65,7 @@ exports.ownedGames = functions.https.onRequest((req, res) => {
   if (!appId) res.send(400);
 
   axios({
-    url: 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=0DFD0511A922896D8064A4EABC32BE08&steamid=76561198017003200&include_appinfo=true&format=json',
+    url: `${STEAM_BASE_URL}/IPlayerService/GetOwnedGames/v0001/?key=0DFD0511A922896D8064A4EABC32BE08&steamid=76561198017003200&include_appinfo=true&format=json`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -83,7 +83,7 @@ exports.gameList = functions.https.onRequest((req, res) => {
   if (!appId) res.send(400);
 
   axios({
-    url: 'http://api.steampowered.com/ISteamApps/GetAppList/v2',
+    url: `${STEAM_BASE_URL}/ISteamApps/GetAppList/v2`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
