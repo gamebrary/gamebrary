77f7b4f1f2ee9372df9c39f35d9af3dec3e9c015
Gamebrary <contact@gamebrary.com>
2022-05-02 21:31:46 -0700

    clean up homepage placeholders

--- src/pages/PublicHomePage.vue
+++ src/pages/PublicHomePage.vue
@@ -1,46 +1,14 @@
 <template lang="html">
   <b-container fluid>
-    [homepage]
-    <b-row>
-      <b-col>
-        <b-card>
-          <h3>Organize</h3>
-          Boards, tags, notes
-        </b-card>
-      </b-col>
-
-      <b-col>
-        <b-card>
-          <h3>Learn</h3>
-        </b-card>
-      </b-col>
-
-      <b-col>
-        <b-card>
-          <h3>Track</h3>
-        </b-card>
-      </b-col>
-    </b-row>
-
-    <hr />
-
-    <b-card class="text-center text-white bg-danger">
-      <h3>Gamebrary Pro</h3>
-      [image]
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      <b-button>Learn more</b-button>
-    </b-card>
-
-    <b-card class="text-center text-white bg-danger">
-      <h3>Gamebrary Pro</h3>
-      [image]
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      <b-button>Learn more</b-button>
-    </b-card>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Gamebrary</h1>
+        <p class="lead font-weight-normal">Your video game collection, organized.</p>
+        <a class="btn btn-outline-secondary" href="#">Get started</a>
+      </div>
+      <div class="product-device shadow-sm d-none d-md-block"></div>
+      <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
 
     <!-- <p>Open source tool to organize video game collections.</p>
     <p>Gamebrary is free, but powered by your support</p>
