3d98ca0afa08d8b7587b97bfd9c4fd6d4d244a3f
Gamebrary <contact@gamebrary.com>
2022-08-29 14:01:27 -0700

    Convert add remove sidebar into a dropdown

--- src/components/AddRemoveGame.vue
+++ src/components/AddRemoveGame.vue
@@ -1,7 +1,5 @@
 <template lang="html">
-  <!-- TODO: use lazy prop for dropdown -->
   <b-dropdown
-    id="dropdown-grouped"
     variant="light"
     right
     no-caret
@@ -10,58 +8,49 @@
       <i class="fa-solid fa-plus fa-fw" />
     </template>
 
-    <p class=" mx-2 my-0 text-center text-muted">
+    <small class="d-block mx-2 my-0 text-center text-muted">
       Add to list
-    </p>
+    </small>
 
     <b-dropdown-divider />
-
-    <pre>{{ gameId }}</pre>
 
     <section
       v-for="board in formattedBoards"
       :key="board.id"
-      class="p-2 bg-white mb-2 field"
+      class="p-2 bg-white mb-2"
     >
-      <div class="text-center">
-        <b-avatar
-          rounded
-          :class="['board-thumbnail', { 'bg-dark' : !board.backgroundColor }]"
-          :title="board.name"
-          text=" "
-          size="32"
-          :style="`
-            background-image: url(${board.backgroundUrl ? board.backgroundUrl : ''});
-            background-color: ${board.backgroundColor ? board.backgroundColor : ''}
-            `"
-          :to="{ name: 'board', params: { id: board.id } }"
-        />
+      <b-avatar
+        rounded
+        :class="['mr-2 mb-2 board-thumbnail', { 'bg-dark' : !board.backgroundColor }]"
+        :title="board.name"
+        text=" "
+        size="32"
+        :style="`
+          background-image: url(${board.backgroundUrl ? board.backgroundUrl : ''});
+          background-color: ${board.backgroundColor ? board.backgroundColor : ''}
+          `"
+        :to="{ name: 'board', params: { id: board.id } }"
+      />
 
-        <br />
+      <small class="text-muted">{{ board.name }}</small>
 
-        <span class="text-muted">{{ board.name }}</span>
-      </div>
-
-      <section
+      <b-button
         v-for="(list, listIndex) in board.lists"
         :key="`${board.id}-${list.name}`"
-        class="d-flex align-items-center justify-content-between mb-2"
+        :variant="isGameInList({ list }) ? 'success' : 'light'"
+        @click="handleClick({ list, listIndex, board })"
+        block
+        size="sm"
       >
-        <b-badge variant="light" class="mr-2">
-          {{ list.games.length }}
-        </b-badge>
-
-        {{ list.name }}
-
-        <b-form-checkbox
-          id="checkbox-1"
-          name="checkbox-1"
-          :checked="isGameInList({ list })"
-          switch
-          @change="handleClick({ list, listIndex, board })"
-          unchecked-value="not_accepted"
+        <i
+          v-if="isGameInList({ list })"
+          class="fa-solid fa-check"
         />
-      </section>
+
+        <small class="text-truncate">
+          {{ list.name }}
+        </small>
+      </b-button>
     </section>
     <b-dropdown-divider />
   </b-dropdown>
@@ -74,19 +63,11 @@ import { getGameCoverUrl } from '@/utils';
 export default {
 
   computed: {
-    ...mapState(['games', 'boards', 'wallpapers', 'user']),
+    ...mapState(['games', 'boards', 'wallpapers']),
 
     // TODO: handle this at action/mutation level OR use getter at least
     formattedBoards() {
-      return this.boards
-        .map((board) => {
-          const backgroundUrl = this.getWallpaperUrl(board.backgroundUrl);
-
-          return {
-            ...board,
-            backgroundUrl,
-          };
-        });
+      return this.boards.map((board) => ({ ...board, backgroundUrl: this.getWallpaperUrl(board.backgroundUrl) }));
     },
 
     game() {
@@ -104,9 +85,7 @@ export default {
 
   methods: {
     isGameInList({ list }) {
-      console.log('list', list);
-      console.log('gameId', this.gameId);
-      return list.games.includes(Number(this.gameId));
+      return list.games?.includes(Number(this.gameId));
     },
 
     getWallpaperUrl(url) {
@@ -119,14 +98,9 @@ export default {
     },
 
     handleClick({ list, listIndex, board }) {
-      console.log('list', list);
-      console.log('listIndex', listIndex);
-      console.log('board', board);
       if (this.isGameInList({ list, gameId: this.game.id })) {
-        console.log('removeGame');
         this.removeGame({ listIndex, boardId: board.id, list });
       } else {
-        console.log('addGame');
         this.addGame({ list, listIndex, boardId: board.id });
       }
     },
