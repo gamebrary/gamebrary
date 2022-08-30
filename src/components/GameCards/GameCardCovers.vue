e1a6edf8b22476e0fa932e095a65d040e19c61f1
Gamebrary <contact@gamebrary.com>
2022-08-29 22:16:49 -0700

    show tags in cover card

--- src/components/GameCards/GameCardCovers.vue
+++ src/components/GameCards/GameCardCovers.vue
@@ -1,8 +1,17 @@
 <template lang="html">
-  <b-img
-    class="cursor-pointer rounded align-self-end card overflow-hidden border-0"
-    :src="coverUrl"
-  />
+  <div class="position-relative cursor-pointer rounded align-self-end card overflow-hidden border-0 mb-2">
+    <b-img
+      :src="coverUrl"
+    />
+
+    <i
+      v-if="tagsApplied.length"
+      class="fas fa-tags position-absolute text-white tag-icon"
+      v-b-tooltip.hover
+      :title="`${tagsApplied.length} tags applied.`"
+      aria-hidden
+    />
+  </div>
 </template>
 
 <script>
@@ -12,3 +21,10 @@ export default {
   mixins: [gameCardMixin],
 };
 </script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+.tag-icon {
+  bottom: .25rem;
+  right: .25rem;
+}
+</style>
