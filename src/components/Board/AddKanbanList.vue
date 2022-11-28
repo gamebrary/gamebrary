eceba39863b0281e710211ff35e02a6e758a2bbe
Gamebrary <contact@gamebrary.com>
2022-11-27 22:06:01 -0700

    Fix add list spacing and auto scroll after adding

--- src/components/Board/AddKanbanList.vue
+++ src/components/Board/AddKanbanList.vue
@@ -1,49 +1,51 @@
 <template lang="html">
-  <b-card
-    v-if="active || empty"
-    class="flex-shrink-0"
-    :bg-variant="darkTheme ? 'info' : 'light'"
-    :text-variant="darkTheme ? 'light' : 'dark'"
-    no-body
-    v-click-outside="reset"
-  >
-    <form
-      ref="addListForm"
-      class="p-2"
-      @submit.stop.prevent="submit"
+  <div class="flex-shrink-0 mx-3">
+    <b-card
+      v-if="active || empty"
+      class="mr-3"
+      :bg-variant="darkTheme ? 'info' : 'light'"
+      :text-variant="darkTheme ? 'light' : 'dark'"
+      no-body
+      v-click-outside="reset"
     >
-      <b-input-group>
-        <b-form-input
-          id="listName"
-          autofocus
-          placeholder="List name"
-          v-model="listName"
-          required
-        />
+      <form
+        ref="addListForm"
+        class="p-2"
+        @submit.stop.prevent="submit"
+      >
+        <b-input-group>
+          <b-form-input
+            id="listName"
+            autofocus
+            placeholder="List name"
+            v-model="listName"
+            required
+          />
 
-        <b-input-group-append>
-          <b-button
-            split
-            variant="primary"
-            :disabled="saving || !listName"
-            @click.stop="submit"
-          >
-            <b-spinner v-if="saving" small />
-            <span v-else>Add</span>
-          </b-button>
-        </b-input-group-append>
-      </b-input-group>
-    </form>
-  </b-card>
+          <b-input-group-append>
+            <b-button
+              split
+              variant="primary"
+              :disabled="saving || !listName"
+              @click.stop="submit"
+            >
+              <b-spinner v-if="saving" small />
+              <span v-else>Add</span>
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </form>
+    </b-card>
 
-  <b-button
-    v-else
-    :variant="darkTheme ? 'dark' : 'light'"
-    @click.stop="showForm"
-    class="mr-3"
-  >
-    <i class="fas fa-plus fa-fw" aria-hidden />
-  </b-button>
+    <b-button
+      v-else
+      :variant="darkTheme ? 'dark' : 'light'"
+      @click.stop="showForm"
+      class="mr-3"
+    >
+      <i class="fas fa-plus fa-fw" aria-hidden />
+    </b-button>
+  </div>
 </template>
 
 <script>
@@ -111,7 +113,7 @@ export default {
 
     scroll() {
       this.$nextTick(() => {
-        const board = document.querySelector('.board');
+        const board = document.querySelector('.viewport');
 
         board.scrollLeft = board.scrollWidth;
       });
