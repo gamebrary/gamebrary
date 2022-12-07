297913caa55f4759aacd8b0fee0c8a5d39837ac6
Gamebrary <contact@gamebrary.com>
2022-12-07 08:55:58 -0700

    Remove translations until they can be regenerated

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,16 +1,16 @@
 /* eslint-disable global-require */
 
 const messages = {
-  ar: require('./ar.json'),
-  de: require('./de.json'),
   en: require('./en.json'),
-  es: require('./es.json'),
-  fr: require('./fr.json'),
-  it: require('./it.json'),
-  ja: require('./ja.json'),
-  pt: require('./pt.json'),
-  ru: require('./ru.json'),
-  'zh-CN': require('./zh-CN.json'),
+  // ar: require('./ar.json'),
+  // de: require('./de.json'),
+  // es: require('./es.json'),
+  // fr: require('./fr.json'),
+  // it: require('./it.json'),
+  // ja: require('./ja.json'),
+  // pt: require('./pt.json'),
+  // ru: require('./ru.json'),
+  // 'zh-CN': require('./zh-CN.json'),
 };
 
 export default messages;
