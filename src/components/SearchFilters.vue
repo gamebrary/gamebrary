244340df48e19d23419d01e637955f5b8e3f618e
Gamebrary <contact@gamebrary.com>
2022-12-15 15:29:40 -0700

    Search filters clean up

--- src/components/SearchFilters.vue
+++ src/components/SearchFilters.vue
@@ -1,121 +1,69 @@
 <!-- TODO: allow multiple filters at once -->
 <!-- TODO: add rating filtering -->
 <template lang="html">
-  <div class="search-filters">
-    <h4>Filter</h4>
-    <b-button
-      block
-      v-b-toggle.genres
-      :variant="filterBy === 'genres' ? 'primary' : normalVariant"
-      class="mb-2"
-    >
-      Genres
-    </b-button>
+  <b-dropdown id="dropdown-grouped" text="Filter" class="mr-2">
+    <b-dropdown-item-button variant="primary">
+      Clear filter
+    </b-dropdown-item-button>
 
-    <b-collapse
-      id="genres"
-      :visible="filterBy === 'genres'"
-      accordion="filters"
-      role="tabpanel"
-    >
-      <b-button
+    <b-dropdown-divider />
+
+    <b-dropdown-group id="genres" header="Genres">
+      <b-dropdown-item-button
         v-for="{ id, name } in $options.GAME_GENRES"
         :key="name"
         block
         size="sm"
-        :variant="isFilterSelected('genres', id) ? 'primary' : normalVariant"
-        class="text-left mb-1"
+        :variant="isFilterSelected('genres', id) ? 'primary' : ''"
         @click="setFilter('genres', id)"
       >
         {{ name }}
-      </b-button>
-    </b-collapse>
+      </b-dropdown-item-button>
+    </b-dropdown-group>
 
-    <b-button
-      block
-      v-b-toggle.player_perspectives
-      class="mb-2"
-      :variant="filterBy === 'player_perspectives' ? 'primary' : normalVariant"
-    >
-      Perspectives
-    </b-button>
-
-    <b-collapse
-      id="player_perspectives"
-      :visible="filterBy === 'player_perspectives'"
-      accordion="filters"
-      role="tabpanel"
-    >
-      <b-button
+    <b-dropdown-group id="dropdown-group-1" header="Perspectives">
+      <b-dropdown-item-button
         v-for="{ id, name } in $options.GAME_PERSPECTIVES"
         :key="name"
         block
         size="sm"
-        :variant="isFilterSelected('player_perspectives', id) ? 'primary' : normalVariant"
-        class="text-left mb-1"
+        :variant="isFilterSelected('player_perspectives', id) ? 'primary' : ''"
         @click="setFilter('player_perspectives', id)"
       >
         {{ name }}
-      </b-button>
-    </b-collapse>
-    <!-- :variant="genresFilter.includes(id) ? 'success' : 'light'" -->
+      </b-dropdown-item-button>
+    </b-dropdown-group>
 
-    <b-button
-      block
-      v-b-toggle.platforms
-      class="mb-2"
-      :variant="filterBy === 'platforms' ? 'primary' : normalVariant"
-    >
-      Platforms
-    </b-button>
+    <b-dropdown-divider />
 
-    <b-collapse
-      id="platforms"
-      :visible="filterBy === 'platforms'"
-      accordion="filters"
-      role="tabpanel"
-    >
-      <b-button
+    <b-dropdown-group id="dropdown-group-2" header="Platforms">
+      <b-dropdown-item-button
         v-for="{ id, name } in platforms"
         :key="id"
-        block
         size="sm"
-        :variant="isFilterSelected('platforms', id) ? 'primary' : normalVariant"
-        class="text-left mb-1"
+        :variant="isFilterSelected('platforms', id) ? 'primary' : ''"
         @click="setFilter('platforms', id)"
       >
         {{ name }}
-      </b-button>
-    </b-collapse>
-
-    <b-button
-      block
-      v-b-toggle.game_modes
-      class="mb-2"
-      :variant="filterBy === 'game_modes' ? 'primary' : normalVariant"
-    >
-      Game modes
-    </b-button>
-
-    <b-collapse
-      id="game_modes"
-      :visible="filterBy === 'game_modes'"
-      accordion="filters"
-      role="tabpanel"
-    >
-      <b-button
+      </b-dropdown-item-button>
+    </b-dropdown-group>
+    <b-dropdown-divider />
+    <b-dropdown-group id="dropdown-group-2" header="Game modes">
+      <b-dropdown-item-button
         v-for="{ id, name } in $options.GAME_MODES"
         :key="id"
-        block
         size="sm"
-        :variant="isFilterSelected('game_modes', id) ? 'primary' : normalVariant"
-        class="text-left mb-1"
+        :variant="isFilterSelected('game_modes', id) ? 'primary' : ''"
         @click="setFilter('game_modes', id)"
       >
         {{ name }}
-      </b-button>
-    </b-collapse>
-  </div>
+      </b-dropdown-item-button>
+    </b-dropdown-group>
+    <b-dropdown-divider />
+    <b-dropdown-item-button>
+      Another Non-grouped Item
+    </b-dropdown-item-button>
+  </b-dropdown>
 </template>
 
 <script>
@@ -137,12 +85,6 @@ export default {
 
     value() {
       return this.$route.query?.value;
-    },
-
-    normalVariant() {
-      return this.darkTheme
-        ? 'secondary'
-        : 'light';
     },
   },
 
@@ -184,12 +126,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" rel="stylesheet/scss" scoped>
-.search-filters {
-  max-height: 80vh;
-  overflow: auto;
-  position: sticky;
-  top: 0;
-}
-</style>
