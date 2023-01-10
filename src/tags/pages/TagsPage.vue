6752bd113d61ed67290b85dd0c556b0c438bf999
Gamebrary <contact@gamebrary.com>
2023-01-10 16:28:17 -0700

    Update tages page layout

--- src/tags/pages/TagsPage.vue
+++ src/tags/pages/TagsPage.vue
@@ -29,8 +29,57 @@
          </b-button>
        </empty-state>
 
-      <div class="field mr-auto ml-auto" v-else>
-        <b-button
+      <b-row v-else>
+        <b-col
+          v-for="({ textColor, bgColor, name, games: taggedGames }, index) in tags"
+          cols="8"
+          :key="name"
+        >
+
+        <b-card class="mb-2">
+          <b-button
+            :style="`background-color: ${bgColor}; color: ${textColor}`"
+            @click="$router.push({ name: 'tag.edit', params: { id: index } })"
+          >
+           {{ name }}
+          </b-button>
+
+          <b-form-row v-if="taggedGames.length" class="mt-2">
+            <b-col
+              v-for="game in taggedGames.slice(0, 5)"
+              :key="game"
+              cols="6"
+              sm="4"
+              md="3"
+              lg="2"
+            >
+              <b-img
+                :src="$options.getImageUrl(games[game], $options.IMAGE_SIZE_COVER_SMALL)"
+                class="cursor-pointer rounded mb-2"
+                fluid
+                @click="$router.push({ name: 'game', params: { id: games[game].id, slug: games[game].slug }})"
+              />
+            </b-col>
+
+            <b-col
+              v-if="taggedGames.length > 5"
+              cols="6"
+              sm="4"
+              md="3"
+              lg="2"
+            >
+              <router-link
+                class="d-flex align-items-center w-100 h-100 bg-light text-dark justify-content-center rounded"
+                :to="{ name: 'tag.edit', params: { id: index } }"
+              >
+                {{ taggedGames.length }} more...
+              </router-link>
+            </b-col>
+          </b-form-row>
+        </b-card>
+        </b-col>
+
+        <!-- <b-button
           v-for="({ textColor, bgColor, name, games }, index) in tags"
           @click="$router.push({ name: 'tag.edit', params: { id: index } })"
           rounded
@@ -40,17 +89,23 @@
           :key="name"
         >
           {{ name }} {{ games.length ? `(${games.length})` : '' }}
-        </b-button>
-      </div>
+        </b-button> -->
+      </b-row>
     </b-container>
   </section>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex';
+import { getImageUrl } from '@/utils';
+import { IMAGE_SIZE_COVER_SMALL } from '@/constants';
+
 import EmptyState from '@/components/EmptyState';
 
 export default {
+  IMAGE_SIZE_COVER_SMALL,
+  getImageUrl,
+
   components: {
     EmptyState,
   },
@@ -62,7 +117,7 @@ export default {
   },
 
   computed: {
-    ...mapState(['tags']),
+    ...mapState(['tags', 'games']),
     ...mapGetters(['darkTheme']),
   },
 
