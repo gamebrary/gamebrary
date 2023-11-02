eea8de236b98b00ee0053d98cbc27b68f43ed293
gamebrary <60666270+gamebrary@users.noreply.github.com>
2023-11-02 11:31:30 -0700

    Codesandbox config files (#271)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    allowedHosts: "all",
+  },
 });
