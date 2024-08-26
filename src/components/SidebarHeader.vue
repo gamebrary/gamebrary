e1cf074b1f8b44dbf28b44fb44eed5fe73ed0f2f
gamebrary <contact@gamebrary.com>
2024-08-26 16:21:06 -0700

    More clean up

--- src/components/SidebarHeader.vue
+++ src/components/SidebarHeader.vue
@@ -1,7 +1,6 @@
 <!-- TODO: standardize title and subtitle -->
-<!-- TODO: finish replacing remaining places -->
 <template lang="html">
-  <div class="d-flex align-items-center justify-content-between p-3 bg-danger">
+  <div class="d-flex align-items-center justify-content-between p-3">
     <slot />
 
     <b-button
@@ -17,30 +16,8 @@
 import { mapGetters } from 'vuex';
 
 export default {
-  // props: {
-  //   profile: {
-  //     type: Object,
-  //     required: true,
-  //   },
-  // },
-
-  // data() {
-  //   return {
-  //     avatarImage: null,
-  //   }
-  // },
-
   computed: {
     ...mapGetters(['darkTheme']),
   },
-
-  async mounted() {
-    // if (this.profile?.avatar) {
-    //   const thumbnailRef = getImageThumbnail(this.profile?.avatar);
-
-    //   this.avatarImage = await this.$store.dispatch('LOAD_FIREBASE_IMAGE', thumbnailRef)
-    //     .catch((e) => {});
-    // }
-  },
-};
+}
 </script>
