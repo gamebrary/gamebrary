b4eecaccd429e59ddfe9146c5cf75761755d3dae
Gamebrary <gamebraryhq@gmail.com>
2025-01-06 11:02:51 -0700

    Improve game progress UI

--- src/components/Game/GameProgress.vue
+++ src/components/Game/GameProgress.vue
@@ -1,29 +1,45 @@
 <template lang="html">
-  <div v-if="user" style="width: 200px">
-    {{ progress }}% Completed
+  <div v-if="user">
+    <div class="alert text-center" role="alert" :class="alertClass">
+      <div v-if="progress === 0 || !progress" class="mb-2">
+        Track your game progress!
+      </div>
 
-    <b-form-input
-      v-model="progress"
-      type="range"
-      max="100"
-      step="1"
-      debounce="500"
-      @update="saveProgress"
-    />
+      <div v-else class="d-flex align-items-center text-center mb-1">
+        <i v-if="progress >= 100" class="fa-sharp fa-solid fa-trophy-star text-warning mr-1" />
 
-    <b-button
-      v-if="progress"
-      :disabled="deleting"
-      variant="transparent"
-      class="text-danger"
-      size="sm"
-      @click="deleteProgress"
-    >
-      <b-spinner small v-if="deleting" />
-      <i v-else class="fas fa-trash fa-fw" aria-hidden />
+        <span :class="{ 'text-warning': progress >= 100 }">
+          {{ progress }}% Completed
+        </span>
 
-      Clear progress
-    </b-button>
+        <b-button
+          v-if="progress"
+          :disabled="deleting"
+          variant="transparent"
+          class="ml-auto"
+          size="sm"
+          @click="deleteProgress"
+        >
+          <b-spinner small v-if="deleting" />
+          <i v-else class="fas fa-trash" aria-hidden />
+        </b-button>
+      </div>
+
+      <b-form-input
+        v-if="showProgressBar || progress"
+        v-model="progress"
+        type="range"
+        class="progress-bar px-2 bg-light"
+        max="100"
+        step="1"
+        debounce="500"
+        @update="saveProgress"
+      />
+
+      <b-button v-else variant="success" @click="showProgressBar = true">
+        Get started
+      </b-button>
+    </div>
   </div>
 
   <div v-else>
@@ -48,6 +64,7 @@ export default {
       progress: 0,
       saving: false,
       deleting: false,
+      showProgressBar: false,
     }
   },
 
@@ -59,6 +76,17 @@ export default {
       if (this.progress < 90) return 'bg-secondary';
 
       return 'bg-success';
+    },
+
+    alertClass() {
+      if (!this.progress) return 'alert-muted';
+      if (this.progress === 0) return 'alert-muted';
+      if (this.progress < 25) return 'alert-info';
+      if (this.progress < 50) return 'alert-warning';
+      if (this.progress < 75) return 'alert-primary';
+      if (this.progress < 90) return 'alert-success';
+
+      return 'alert-warning';
     },
   },
 
@@ -82,6 +110,8 @@ export default {
     async deleteProgress() {
       try {
         const { id, name } = this.game;
+
+        this.showProgressBar = false;
 
         this.deleting = true;
 
@@ -119,3 +149,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.progress-bar {
+  position: relative;
+  height: 24px;
+  border-radius: 4px;
+
+  /* box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.2); */
+}
+</style>
