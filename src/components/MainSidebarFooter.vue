13f40ae76bff4c9333e65382e19c4251e81fdb0a
Gamebrary <gamebraryhq@gmail.com>
2025-01-08 10:33:54 -0700

    Clean up sidebar footer

--- src/components/MainSidebarFooter.vue
+++ src/components/MainSidebarFooter.vue
@@ -1,58 +1,51 @@
 <template>
-  <div class="p-3">
-    <div class="mt-1 text-center d-flex justify-content-between">
-      <div class="d-flex justify-content-between align-items-center">
-        <img
-          src="/logo.png"
-          alt=""
-          height="26"
-          class="mr-2"
-        />
+  <div class="p-3 text-center d-flex align-items-center justify-content-center flex-column">
+    <div>
+      <b-button
+        :variant="darkTheme ? 'dark' : 'light'"
+        href="https://github.com/romancm/gamebrary/"
+        target="_blank"
+        title="GitHub"
+        size="lg"
+        v-b-tooltip.hover
+      >
+        <i class="fa-brands fa-github" />
+      </b-button>
 
-        &copy; {{ currentYear }} Gamebrary
-      </div>
+      <b-button
+        v-b-toggle.keyboard-shortcuts-sidebar
+        :variant="darkTheme ? 'dark' : 'light'"
+        title="Keyboard Shortcuts"
+        v-b-tooltip.hover
+        size="lg"
+      >
+        <i class="fa-solid fa-command" />
+      </b-button>
 
-      <div>
-        <b-button
-          :variant="darkTheme ? 'dark' : 'light'"
-          href="https://github.com/romancm/gamebrary/"
-          target="_blank"
-          title="GitHub"
-          v-b-tooltip.hover
-        >
-          <i class="fa-brands fa-github" />
-        </b-button>
+      <b-button
+        :to="{ name: 'help' }"
+        id="help"
+        title="Help"
+        :variant="darkTheme ? 'dark' : 'light'"
+        size="lg"
+        v-b-tooltip.hover
+      >
+        <i class="fa-regular fa-circle-info" />
+      </b-button>
 
-        <b-button
-          v-b-toggle.keyboard-shortcuts-sidebar
-          :variant="darkTheme ? 'dark' : 'light'"
-          title="Keyboard Shortcuts"
-          v-b-tooltip.hover
-        >
-          <i class="fa-solid fa-command" />
-        </b-button>
-
-        <b-button
-          :to="{ name: 'help' }"
-          id="help"
-          title="Help"
-          :variant="darkTheme ? 'dark' : 'light'"
-          v-b-tooltip.hover
-        >
-          <i class="fa-regular fa-circle-info" />
-        </b-button>
-
-        <b-button
-          @click="toggleTheme"
-          :variant="darkTheme ? 'dark' : 'light'"
-          v-b-tooltip.hover
-          title="Toggle theme"
-        >
-          <i v-if="darkTheme" class="fa-solid fa-sun" />
-          <i v-else class="fa-solid fa-moon" />
-        </b-button>
-      </div>
+      <b-button
+        @click="toggleTheme"
+        :variant="darkTheme ? 'dark' : 'light'"
+        size="lg"
+        v-b-tooltip.hover
+        title="Toggle theme"
+      >
+        <i v-if="darkTheme" class="fa-solid fa-sun" />
+        <i v-else class="fa-solid fa-moon" />
+      </b-button>
     </div>
+
+    <div>&copy; {{ currentYear }} Gamebrary</div>
   </div>
 </template>
 
@@ -72,19 +65,14 @@ export default {
 
   methods: {
     async toggleTheme() {
-      const { settings } = this;
-      const darkTheme = settings?.darkTheme || false;
-
-      const payload = {
-        ...settings,
-        darkTheme: !darkTheme,
-      };
-
-      await this.$store.dispatch('SAVE_SETTINGS', payload)
-        .catch(() => {
-          this.$bvToast.toast('There was an error saving your settings', { variant: 'danger' });
-          this.saving = false;
+      try {
+        await this.$store.dispatch('SAVE_SETTINGS', {
+          ...this.settings,
+          darkTheme: !this.settings?.darkTheme,
         });
+      } catch (error) {
+        this.$bvToast.toast('There was an error saving your settings', { variant: 'danger' });
+      }
     },
   },
 }
