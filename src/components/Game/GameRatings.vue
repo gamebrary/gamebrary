33d5e1d6cdb5f8fe4fe7bb657d4378963b5595a3
gamebrary <contact@gamebrary.com>
2025-01-29 23:04:29 -0700

    Show all ratings

--- src/components/Game/GameRatings.vue
+++ src/components/Game/GameRatings.vue
@@ -1,28 +1,14 @@
 <template lang="html">
-  <b-dropdown v-if="ratings" variant="link" toggle-class="p-0" no-caret>
-    <template #button-content>
-      <b-img
-        :src="`/img/age-ratings/${ratings.rating}.${ratings.logoFormat || 'png'}`"
-        :alt="ratings.rating"
-        :key="ratings.rating"
-        height="60"
-      />
-
-    </template>
-    
-    <b-dropdown-text>
-      <div class="d-flex align-items-start">
-        <b-img
-          v-for="{ rating, logoFormat, description } in gameRatings"
-          :title="description"
-          :key="rating"
-          class="mr-2"
-          :src="`/img/age-ratings/${rating}.${logoFormat || 'png'}`"
-          height="60"
-        />
-      </div>
-    </b-dropdown-text>
-  </b-dropdown>
+  <div>
+    <b-img
+      v-for="{ rating, logoFormat, description } in gameRatings"
+      :title="description"
+      :key="rating"
+      class="mr-2"
+      :src="`/img/age-ratings/${rating}.${logoFormat || 'png'}`"
+      height="60"
+    />
+  </div>
 </template>
 
 <script>
